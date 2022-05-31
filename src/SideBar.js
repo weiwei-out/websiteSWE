236bbf17f1109ec020419911a83019394a7e741a
Andrew Wei <andrewwei913@gmail.com>
2022-05-31 11:46:17 -0700

    01 SideBar

---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+function SideBar() {
+  return (
+    <div>
+      <div className="bg-red1 w-sideBar h-sideBar rounded-tr-2xl">a</div>
+      <div className="bg-red2 w-sideBar h-sideBar">b</div>
+      <div className="bg-red3 w-sideBar h-sideBar">c</div>
+      <div className="bg-red4 w-sideBar h-sideBar rounded-br-2xl">d</div>
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red1: "#FB4D4D",
+        red2: "#DF4444",
+        red3: "#AA3232",
+        red4: "#8A2525",
+        white: "#FFFFFF",
+        bg1: "#121D26",
+        bg2: "#192631",
+      },
+      width: {
+        sideBar: "var(--sideBar)",
+      },
+      height: {
+        sideBar: "var(--sideBar)",
+      },
+    },
+  },
+  plugins: [],
+};
